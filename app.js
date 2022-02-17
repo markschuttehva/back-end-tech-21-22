f9f90ef1cbd06d161626bb3823ac84c76416ded1
Mark <mark.schutte@hva.nl>
2022-02-17 14:18:08 +0100

    add routes js6

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.all('/', (req, res) => {
+    res.status(200).send('<h1>200! Hello world</h1>');
+});
 
-app.get('/about', function (req, res) {
-    res.send('about')
-})
+app.all('/about', (req, res) => {
+    res.status(200).send('<h1>200! about</h1>');
+});
 
-app.get('/register', function (req, res) {
-    res.send('register')
-})
+app.all('/register', (req, res) => {
+    res.status(200).send('<h1>200! register</h1>');
+});
+
+app.all('/login', (req, res) => {
+    res.status(200).send('<h1>200! login</h1>');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+});
 
 app.listen(3000)
