d6a5f022a755f3ee83fe02f399912d24dd12bd10
Mark <mark.schutte@hva.nl>
2022-02-18 22:54:57 +0100

    adding handlebars, mapping and other things

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+// import express from 'express';
+// import { engine } from 'express-handlebars';
+// app.set('views', './views');
+
+/* routes*/
 const express = require('express');
 const app = express();
+/*
+https://www.youtube.com/watch?v=hh45sR9WNH8&ab_channel=ChristianHur
+https://github.com/ChristianHur/152-150-Web-Programming-2/tree/master/unit6
+*/
+const exphbs = require('express-handlebars').engine; 
+
 const PORT = process.env.PORT || 1337;
 
+/* voor de statische bestanden zoals css en afbeeldingen */
+app.use(express.static(__dirname + "/public"));
+
+/* handlebars settings */
+app.set('view engine', "hbs");
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+}));
+
 app.get('/', onHome);
-app.get('/', onAbout);
-app.get('/', onRegister);
-app.get('/', onLogin);
+app.get('/about', onAbout);
+app.get('/register', onRegister);
+app.get('/login', onLogin);
 app.get('*', notFound)
 
 function onHome (req, res) {
-    res.send('<h1>home</h1>');
+    res.render("main");
 }
 
 function onAbout (req, res) {
@@ -43,3 +66,4 @@ app.listen(PORT, () => {
 https://handlebarsjs.com/installation/#npm-or-yarn-recommended
 https://expressjs.com/
 */
+
