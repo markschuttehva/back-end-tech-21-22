4c1484b01632935d9265cbc49c59de8151862400
Mark <mark.schutte@hva.nl>
2022-03-02 02:01:35 +0100

    adding css for header and footer

---
+++ controllers/otherfunctions.js
@@ -0,0 +1,21 @@
+/*
+Seen at the udemy cursus
+https://www.udemy.com/course/html-javascript-bootstrap-certification-course/
+- used to write a line of text or variables
+document.write();
+- pop up menu
+window.alert();
+- change value of the id
+document.getelementbyid(id)
+- change the inner html
+document.getElementById(id).innerHTML
+- 1 value of the auto object
+const auto = {type:"bmw", model:"1 serie"}
+document.getElementById(id).innerHTML = auto.type
+document.getElementById(id).innerHTML = auto.type + auto.model
+- random nummer betweentussen 0-1
+document.getElementById(id).innerHTML = Math.random();
+- array
+const array = ["list", "of", "array"];
+*/
+
